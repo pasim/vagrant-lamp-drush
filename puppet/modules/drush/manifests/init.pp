09dcd1364c0b66540c66fe06ba221b619b529047
Janis Janovskis <janis.janovskis@passivemanagement.net>
2015-11-24 01:33:34 +0200

    Adding correct Drush support

--- puppet/modules/drush/manifests/init.pp
+++ puppet/modules/drush/manifests/init.pp
@@ -7,13 +7,20 @@ class drush::install {
 # pear.drush.org without complaint.
   pear::package { "PEAR": }
   pear::package { "Console_Table": }
-
-  # Version numbers are supported.
-  pear::package { "drush":
-    repository => "pear.drush.org",
-  }
 }
 
 class drush {
   include drush::install
+
+  exec{ 'download-drush':
+    command   => "/usr/bin/wget -q https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar -O /usr/local/bin/drush",
+    creates   => "/usr/local/bin/drush",
+    user => root,
+  }
+
+  file{ "/usr/local/bin/drush":
+    mode   => 0777,
+    require => Exec['download-drush'],
+  }
+
 }
