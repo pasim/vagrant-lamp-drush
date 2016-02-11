3cc37a4fd0b1ad877c7c35b43cfc993bb45ba396
pasim <janis.janovskis@gmail.com>
2016-02-11 22:41:13 +0200

    Update init.pp
    Ads COMPOSER_HOME environmental variable

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -8,5 +8,7 @@ class composer {
     command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
     path    => '/usr/bin:/usr/sbin',
     require => Package['curl'],
+    creates => '/usr/local/bin/composer',
+    environment => ["COMPOSER_HOME=/home/vagrant"],
   }
 }
