9cf5c0e57bc5c27ca8c92954bce2c47d1b64357c
pasim <janis.janovskis@gmail.com>
2016-03-11 00:07:24 +0200

    Removing redundant PEAR

--- puppet/modules/drush/manifests/init.pp
+++ puppet/modules/drush/manifests/init.pp
@@ -1,16 +1,4 @@
-class drush::install {
-
-  include pear
-
-# If no version number is supplied, the latest stable release will be
-# installed. In this case, upgrade PEAR to 1.9.2+ so it can use
-# pear.drush.org without complaint.
-  pear::package { "PEAR": }
-  pear::package { "Console_Table": }
-}
-
 class drush {
-  include drush::install
 
   exec{ 'download-drush':
     command   => "/usr/bin/wget -q https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar -O /usr/local/bin/drush",
